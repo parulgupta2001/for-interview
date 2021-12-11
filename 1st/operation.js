54270033283d813d287313969c71bea887dbd02b
Parul Gupta <guptaparul1002@gmail.com>
2021-12-11 19:31:27 +0530

    final commit

--- 1st/operation.js
+++ 1st/operation.js
@@ -1,17 +1,37 @@
 var input = document.querySelectorAll(".input");
 var button1 = document.querySelector("#button1");
+var button2 = document.querySelector("#button2");
+var button3 = document.querySelector("#button3");
+var button4 = document.querySelector("#button4");
 var output = document.querySelector("#output");
 
 
 
 
 
-function clickEventHandler (){
+function addition (){
     var add = Number(input[0].value) + Number(input[1].value) ;
-    console.log("add");
+    output.innerText = add;
+}
+
+function subtraction (){
+    var subtract = Number(input[0].value) - Number(input[1].value) ;
+    output.innerText = subtract;
+}
+
+function multiplication (){
+    var multiply = Number(input[0].value) * Number(input[1].value) ;
+    output.innerText = multiply;
+}
+
+function division (){
+    var divide = Number(input[0].value) / Number(input[1].value) ;
+    output.innerText = divide;
 }
 
 
 
-
-button1.addEventListener("click" , clickEventHandler)
+button1.addEventListener("click" , addition);
+button2.addEventListener("click" , subtraction);
+button3.addEventListener("click" , multiplication);
+button4.addEventListener("click" , division);
